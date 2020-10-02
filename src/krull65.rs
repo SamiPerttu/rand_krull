f81475f91befb32e61a7f175138bf9fff922bc05
SamiPerttu <Sami.Perttu@gmail.com>
2020-10-02 14:37:41 +0300

    Improved design.

--- src/krull65.rs
+++ src/krull65.rs
@@ -17,10 +17,10 @@ pub struct Krull65 {
     a0: u64,
     /// LCG A state, high 64 bits.
     a1: u64,
-    /// LCG B state. Phase difference (B - A) encodes high 64 bits of the stream.
-    b: u64,
-    /// Stream, low 64 bits.
-    c: u64,
+    /// LCG B state, low 64 bits.
+    b0: u64,
+    /// LCG b state, high 64 bits.
+    b1: u64,
 }
 
 // As recommended, this Debug implementation does not expose internal state.
@@ -30,60 +30,70 @@ impl core::fmt::Debug for Krull65 {
     }
 }
 
-/// LCG origin is the state at position 0.
-#[inline] fn origin_a0(c: u64) -> u64 {
-    c
+#[inline] fn origin_a0() -> u64 {
+    0
 }
 
-/// LCG origin is the state at position 0.
-#[inline] fn origin_b(c: u64) -> u64 {
-    !c
+#[inline] fn origin_a_128() -> u128 {
+    origin_a0() as u128
+}
+
+#[inline] fn origin_b0() -> u64 {
+    1
+}
+
+#[inline] fn origin_b_128() -> u128 {
+    origin_b0() as u128
 }
 
 impl Krull65 {
 
+    pub fn print(&self) {
+        println!("A {:16x}.. B {:16x}.. output {:16x} position {}", self.a1, self.b1, self.get(), self.position());
+    }
+
     #[inline] fn multiplier_a(&self) -> u64 {
-        super::LCG_M65_2 as u64
+        super::LCG_M65_1 as u64
     }
 
     #[inline] fn multiplier_a_128(&self) -> u128 {
-        super::LCG_M65_2
+        super::LCG_M65_1
     }
 
     #[inline] fn multiplier_b(&self) -> u64 {
-        super::LCG_M64_1
+        super::LCG_M65_4 as u64
     }
 
-    #[inline] fn increment_a0(&self) -> u64 {
-        self.c | 1
-    }
-
-    #[inline] fn increment_a1(&self) -> u64 {
-        0
+    #[inline] fn multiplier_b_128(&self) -> u128 {
+        super::LCG_M65_4
     }
 
     #[inline] fn increment_a_128(&self) -> u128 {
-        self.increment_a0() as u128 | ((self.increment_a1() as u128) << 64)
+        // Pick a big constant that is odd.
+        super::LCG_M128_1
     }
 
-    #[inline] fn increment_b(&self) -> u64 {
-        (self.c << 1) | 1
-    }
-
-    #[inline] fn origin_a0(&self) -> u64 {
-        origin_a0(self.c)
-    }
-
-    #[inline] fn origin_a_128(&self) -> u128 {
-        origin_a0(self.c) as u128
-    }
-
-    #[inline] fn origin_b(&self) -> u64 {
-        origin_b(self.c)
+    #[inline] fn increment_b_128(&self) -> u128 {
+        // Pick a big constant that is odd.
+        super::LCG_M128_4
     }
 
     #[inline] fn a_128(&self) -> u128 {
         self.a0 as u128 | ((self.a1 as u128) << 64)
+    }
+
+    #[inline] fn set_a_128(&mut self, a: u128) {
+        self.a0 = a as u64;
+        self.a1 = (a >> 64) as u64;
+    }
+
+    #[inline] fn b_128(&self) -> u128 {
+        self.b0 as u128 | ((self.b1 as u128) << 64)
+    }
+
+    #[inline] fn set_b_128(&mut self, b: u128) {
+        self.b0 = b as u64;
+        self.b1 = (b >> 64) as u64;
     }
 
     /// Advances to the next state.
@@ -93,9 +103,11 @@ impl Krull65 {
         let a = (self.a0 as u128) * (self.multiplier_a() as u128) + self.increment_a_128();
         self.a1 = ((a >> 64) as u64) + self.a1 * self.multiplier_a() + self.a0;
         self.a0 = a as u64;
-        self.b = self.b * self.multiplier_b() + self.increment_b();
+        let b = (self.b0 as u128) * (self.multiplier_b() as u128) + self.increment_b_128();
+        self.b1 = ((b >> 64) as u64) + self.b1 * self.multiplier_b() + self.b0;
+        self.b0 = b as u64;
 
-        // Here's the equivalent stepping code in 128-bit math.
+        // Here's the stepping code for A in 128-bit math.
         // let a = self.a_128() * self.multiplier_a_128() + self.increment_a_128();
         // self.a1 = (a >> 64) as u64;
         // self.a0 = a as u64;
@@ -103,18 +115,41 @@ impl Krull65 {
 
     /// Returns the current 64-bit output.
     #[wrappit] #[inline] fn get(&self) -> u64 {
-        // Take 64 high bits from A and 56 high bits from B.
-        // As we're XORing different bits of the LCGs together,
+        // Krull65 algorithm consists of two 128-bit LCGs advancing in synchrony.
+        // The LCGs A and B, which are always run with the same constants, realize
+        // two fixed sequences of length 2**128. The stream constant is chosen
+        // by positioning B against A.
+
+        // As our starting point, we take the XOR of some high quality bits from A and B.
+        // Choose high 64 bits from B and the next to highest 64 bits from A.
+        // As we're mixing different bits of the LCGs together,
         // and the rest of the pipeline is bijective, this guarantees
         // equidistribution with each 64-bit output appearing 2**64 times in each stream.
-        // In the worst case, the lag design causes a subtle pairwise bias between sequences
-        // due to collisions that starts being detectable at around 2**62 samples.
-        // The (very unlikely) worst case is where A is identical and only B is different.
-        let x = self.a1 ^ (self.b >> 8);
-        // The output stage is built to pass tests also as an indexed RNG.
-        let x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        let x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        let x = (x ^ (x >> 31)) * 0xd6e8feb86659fd93;
+        let x = self.b1 ^ (self.a1 << 1) ^ (self.a0 >> 63);
+
+        // We can examine our chosen worst case of the user XORing two streams X and Y.
+        // At this point in the pipeline, pairwise correlations between X and Y
+        // can be measured easily, as they are just autocorrelations of B: A is identical.
+        // So the sequence X XOR Y is the XOR of B with a lagged copy of itself.
+        // Autocorrelation of an LCG is indicated by the lowest differing bit.
+        // For instance, if streams X and Y share an identical lowest 32 bits,
+        // then statistical tests fail at 256 MB, but if they share 48 bits,
+        // then statistical tests fail at 2 MB already.
+        // Fortunately, there are only a vanishing fraction of pairwise streams
+        // where the output hash has to do significant work to remove the correlations.
+        // In the next table we can see how hashing improves the result
+        // with some statistical failures of X XOR Y investigated with PractRand.
+        // The output hash is intended to pass tests also as an indexed RNG.
+        //
+        // Lowest differing bit    32     64     96     127
+        // ------------------------------------------------
+        // No hashing            256MB    1MB    1MB    1MB
+        // 1 round              >256GB    1GB    1MB    1MB
+        // 2 rounds                ?      ?    >32GB    1MB
+        // 3 rounds                ?      ?      ?    >64GB
+        let x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9; // round 1
+        let x = (x ^ (x >> 27)) * 0x94d049bb133111eb; // round 2
+        let x = (x ^ (x >> 31)) * 0xd6e8feb86659fd93; // round 3
         x ^ (x >> 32)
     }
 
@@ -128,7 +163,7 @@ impl Krull65 {
     /// Creates a new Krull65 RNG.
     /// Stream and position are set to 0.
     pub fn new() -> Self {
-        Krull65 { a0: origin_a0(0), a1: 0, b: origin_b(0), c: 0 }
+        Krull65 { a0: origin_a0(), a1: 0, b0: origin_b0(), b1: 0 }
     }
 
     /// Creates a new Krull65 RNG.
@@ -143,49 +178,44 @@ impl Krull65 {
     /// Jumps forward (if steps > 0) or backward (if steps < 0) or does nothing (if steps = 0).
     /// The stream wraps around, so signed steps can be interpreted as unsigned.
     pub fn jump(&mut self, steps: i128) {
-        let a = crate::lcg::get_state(self.multiplier_a_128(), self.increment_a_128(), self.a_128(), steps as u128);
-        self.a1 = (a >> 64) as u64;
-        self.a0 = a as u64;
-        self.b = crate::lcg::get_state(self.multiplier_b(), self.increment_b(), self.b, steps as u64);
+        self.set_a_128(crate::lcg::get_state(self.multiplier_a_128(), self.increment_a_128(), self.a_128(), steps as u128));
+        self.set_b_128(crate::lcg::get_state(self.multiplier_b_128(), self.increment_b_128(), self.b_128(), steps as u128));
     }
 
     /// Returns current position in stream. The full state of the generator is (stream, position).
     pub fn position(&self) -> u128 {
-        // Position is encoded wholly in LCG A.
-        super::lcg::get_iterations(self.multiplier_a_128(), self.increment_a_128(), self.origin_a_128(), self.a_128())
+        // Position is encoded in A.
+        super::lcg::get_iterations(self.multiplier_a_128(), self.increment_a_128(), origin_a_128(), self.a_128())
     }
 
     /// Sets position in stream.
     pub fn set_position(&mut self, position: u128) {
         let delta = position.wrapping_sub(self.position());
-        self.b = crate::lcg::get_state(self.multiplier_b(), self.increment_b(), self.b, delta as u64);
-        let a = crate::lcg::get_state(self.multiplier_a_128(), self.increment_a_128(), self.a_128(), delta as u128);
-        self.a1 = (a >> 64) as u64;
-        self.a0 = a as u64;
+        self.jump(delta as i128);
     }
 
     /// Resets stream position to 0. Equivalent to set_position(0).
     #[inline]
     pub fn reset(&mut self) {
-        self.set_position(0);
+        self.a0 = origin_a0();
+        self.a1 = 0;
+        self.b0 = origin_b0();
+        self.b1 = 0;
     }
 
     /// Returns current stream. The full state of the generator is (stream, position).
     #[inline]
     pub fn stream(&self) -> u128 {
         let a_n = self.position();
-        let b_n = super::lcg::get_iterations(self.multiplier_b(), self.increment_b(), self.origin_b(), self.b);
-        // High 64 bits of the stream are encoded in the phase difference (B - A).
-        let c_2 = b_n.wrapping_sub(a_n as u64);
-        self.c as u128 | ((c_2 as u128) << 64)
+        let b_n = super::lcg::get_iterations(self.multiplier_b_128(), self.increment_b_128(), origin_b_128(), self.b_128());
+        // The stream is encoded as the phase difference (B - A).
+        b_n.wrapping_sub(a_n)
     }
 
     /// Sets stream and initializes position to 0.
     pub fn set_stream(&mut self, stream: u128) {
-        self.c = stream as u64;
-        self.a0 = self.origin_a0();
-        self.a1 = 0;
-        self.b = super::lcg::get_state(self.multiplier_b(), self.increment_b(), self.origin_b(), (stream >> 64) as u64);
+        self.reset();
+        self.set_b_128(crate::lcg::get_state(self.multiplier_b_128(), self.increment_b_128(), origin_b_128(), stream));
     }
 }
 
@@ -266,7 +296,7 @@ impl SeedableRng for Krull65 {
             assert_eq!(pos1, krull1.position());
 
             let n = 1 + (rnd() & 0x3ff);
-            for i in 0 .. n { krull1.next_u64(); }
+            for _ in 0 .. n { krull1.next_u64(); }
             assert_eq!(pos1 + n, krull1.position());
 
             assert_eq!(seed, krull1.stream());
